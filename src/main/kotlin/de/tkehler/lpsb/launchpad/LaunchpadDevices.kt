c82a4b87e159ded83af90665b2141c7a9a8b79fe
Thibaud Kehler <mail@tkehler.de>
2019-04-15 23:48:36 +0200

    edited gradle config

---
+++ src/main/kotlin/de/tkehler/lpsb/launchpad/LaunchpadDevices.kt
@@ -0,0 +1,17 @@
+package de.tkehler.lpsb.launchpad
+
+
+
+
+interface LaunchpadDevice {
+
+
+    public class Info {
+        fun equals(obj: Object): Boolean{
+            //TODO
+            return false
+        }
+    }
+}
+
+
---
+++ src/main/kotlin/de/tkehler/lpsb/launchpad/LaunchpadSystem.kt
@@ -0,0 +1,10 @@
+package de.tkehler.lpsb.launchpad
+
+object LaunchpadSystem {
+    fun getLaunchpadDevice() : LaunchpadDevice {
+        //TODO
+        return LaunchpadDevice()
+    }
+}
+
+
